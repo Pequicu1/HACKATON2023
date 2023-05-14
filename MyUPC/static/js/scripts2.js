b73516d4743e23deddf78ab2fccca4d5438becbb
Ivan <ivbuirez@gmail.com>
2023-05-14 07:16:10 +0200

    Arreglado tu sucio frontend

--- MyUPC/static/js/scripts2.js
+++ MyUPC/static/js/scripts2.js
@@ -1,5 +1,5 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByClassName("todo");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
